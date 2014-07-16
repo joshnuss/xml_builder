aea8272547911b5cc6a3809470880cab69afd2f4
Josh Nussbaum <joshnuss@gmail.com>
2014-07-16 12:09:14 -0400

    Added support for attributes

--- lib/builder.ex
+++ lib/builder.ex
@@ -2,6 +2,15 @@ defmodule Builder do
   def xml(name),
     do: "<#{name}/>"
 
+  def xml(name, attrs) when is_map(attrs) and map_size(attrs) == 0,
+    do: xml(name)
+
+  def xml(name, attrs) when is_map(attrs),
+    do: "<#{name} #{build_attributes(attrs)}/>"
+
   def xml(name, content),
     do: "<#{name}>#{content}</#{name}>"
+
+  defp build_attributes(attrs),
+    do: Enum.map_join(attrs, " ", fn {k,v} -> ~s/#{k}="#{v}"/ end)
 end
--- test/builder_test.exs
+++ test/builder_test.exs
@@ -8,4 +8,9 @@ defmodule BuilderTest do
   test "element with content" do
     assert Builder.xml(:person, "Josh") == "<person>Josh</person>"
   end
+
+  test "element with attributes" do
+    assert Builder.xml(:person, %{occupation: "Developer", city: "Montreal"}) == ~s|<person city="Montreal" occupation="Developer"/>|
+    assert Builder.xml(:person, %{}) == ~s|<person/>|
+  end
 end
