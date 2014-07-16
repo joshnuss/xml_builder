c5716360569d50a793e91b09a77cbdaa457e4df3
Josh Nussbaum <joshnuss@gmail.com>
2014-07-16 12:15:29 -0400

    Added Builder.xml/3

--- lib/builder.ex
+++ lib/builder.ex
@@ -11,6 +11,15 @@ defmodule Builder do
   def xml(name, content),
     do: "<#{name}>#{content}</#{name}>"
 
+  def xml(name, attrs, nil),
+    do: xml(name, attrs)
+
+  def xml(name, attrs, content) when is_map(attrs) and map_size(attrs) == 0,
+    do: "<#{name}>#{content}</#{name}>"
+
+  def xml(name, attrs, content),
+    do: "<#{name} #{build_attributes(attrs)}>#{content}</#{name}>"
+
   defp build_attributes(attrs),
     do: Enum.map_join(attrs, " ", fn {k,v} -> ~s/#{k}="#{v}"/ end)
 end
--- test/builder_test.exs
+++ test/builder_test.exs
@@ -13,4 +13,11 @@ defmodule BuilderTest do
     assert Builder.xml(:person, %{occupation: "Developer", city: "Montreal"}) == ~s|<person city="Montreal" occupation="Developer"/>|
     assert Builder.xml(:person, %{}) == ~s|<person/>|
   end
+
+  test "element with attributes and content" do
+    assert Builder.xml(:person, %{occupation: "Developer", city: "Montreal"}, "Josh") == ~s|<person city="Montreal" occupation="Developer">Josh</person>|
+    assert Builder.xml(:person, %{occupation: "Developer", city: "Montreal"}, nil) == ~s|<person city="Montreal" occupation="Developer"/>|
+    assert Builder.xml(:person, %{}, "Josh") == "<person>Josh</person>"
+    assert Builder.xml(:person, %{}, nil) == "<person/>"
+  end
 end
