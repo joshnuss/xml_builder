43a4f4e3df78bfee067f6e1075d07684cd1d4a52
Josh Nussbaum <joshnuss@gmail.com>
2014-07-17 03:11:50 -0400

    Refactor to two-step process. build then generate

--- lib/xml_builder.ex
+++ lib/xml_builder.ex
@@ -4,33 +4,33 @@ defmodule XmlBuilder do
 
   ## Examples
 
-      iex> XmlBuilder.doc(:person)
+      iex> XmlBuilder.doc(:person) |> XmlBuilder.generate
       "<?xml version=\\\"1.0\\\"><person/>"
 
-      iex> XmlBuilder.doc(:person, "Josh")
+      iex> XmlBuilder.doc(:person, "Josh") |> XmlBuilder.generate
       "<?xml version=\\\"1.0\\\"><person>Josh</person>"
 
-      iex> XmlBuilder.element(:person, "Josh")
+      iex> XmlBuilder.element(:person, "Josh") |> XmlBuilder.generate
       "<person>Josh</person>"
 
-      iex> XmlBuilder.element(:person, %{occupation: "Developer"}, "Josh")
+      iex> XmlBuilder.element(:person, %{occupation: "Developer"}, "Josh") |> XmlBuilder.generate
       "<person occupation=\\\"Developer\\\">Josh</person>"
   """
 
   def doc(name_or_tuple),
-    do: doc_type <> element(name_or_tuple)
+    do: [:_doc_type | element(name_or_tuple) |> List.wrap]
 
   def doc(name, attrs_or_content),
-    do: doc_type <> element(name, attrs_or_content)
+    do: [:_doc_type | [element(name, attrs_or_content)]]
 
   def doc(name, attrs, content),
-    do: doc_type <> element(name, attrs, content)
+    do: [:_doc_type | [element(name, attrs, content)]]
 
   def element(name) when is_bitstring(name) or is_atom(name),
     do: element({name})
 
   def element(list) when is_list(list),
-    do: build_content(list)
+    do: Enum.map(list, &element/1)
 
   def element({name}),
     do: element({name, nil, nil})
@@ -41,37 +41,46 @@ defmodule XmlBuilder do
   def element({name, content}),
     do: element({name, nil, content})
 
-  def element({name, attrs, content}) when (attrs == nil or map_size(attrs) == 0) and (content==nil or (is_list(content) and length(content)==0)),
-    do: "<#{name}/>"
-
-  def element({name, attrs, content}) when content==nil or (is_list(content) and length(content)==0),
-    do: "<#{name} #{build_attributes(attrs)}/>"
-
-  def element({name, attrs, content}) when attrs == nil or map_size(attrs) == 0,
-    do: "<#{name}>#{build_content(content)}</#{name}>"
-
-  def element({name, attrs, content}),
-    do: "<#{name} #{build_attributes(attrs)}>#{build_content(content)}</#{name}>"
-
   def element(name, attrs) when is_map(attrs),
     do: element({name, attrs, nil})
 
   def element(name, content),
     do: element({name, nil, content})
 
+  def element({name, attrs, content}) when is_list(content),
+    do: {name, attrs, Enum.map(content, &element/1)}
+
   def element(name, attrs, content),
     do: element({name, attrs, content})
 
-  def doc_type,
+  def element(tuple={_name, _attrs, _content}),
+    do: tuple
+
+  def generate(:_doc_type),
     do: ~s|<?xml version="1.0">|
 
-  defp build_content(children) when is_list(children),
-    do: Enum.map_join(children, "", &element/1)
+  def generate(list) when is_list(list),
+    do: Enum.map_join(list, &generate/1)
 
-  defp build_content(content),
+  def generate({name, attrs, content}) when (attrs == nil or map_size(attrs) == 0) and (content==nil or (is_list(content) and length(content)==0)),
+    do: "<#{name}/>"
+
+  def generate({name, attrs, content}) when content==nil or (is_list(content) and length(content)==0),
+    do: "<#{name} #{generate_attributes(attrs)}/>"
+
+  def generate({name, attrs, content}) when attrs == nil or map_size(attrs) == 0,
+    do: "<#{name}>#{generate_content(content)}</#{name}>"
+
+  def generate({name, attrs, content}),
+    do: "<#{name} #{generate_attributes(attrs)}>#{generate_content(content)}</#{name}>"
+
+  defp generate_content(children) when is_list(children),
+    do: Enum.map_join(children, "", &generate/1)
+
+  defp generate_content(content),
     do: escape(content)
 
-  defp build_attributes(attrs),
+  defp generate_attributes(attrs),
     do: Enum.map_join(attrs, " ", fn {k,v} -> "#{k}=#{quote_attribute_value(v)}" end)
 
   defp quote_attribute_value(val) when not is_bitstring(val),
--- test/xml_builder_test.exs
+++ test/xml_builder_test.exs
@@ -2,50 +2,65 @@ defmodule XmlBuilderTest do
   use ExUnit.Case
   doctest XmlBuilder
 
+  def document(name),
+    do: XmlBuilder.doc(name) |> XmlBuilder.generate
+
+  def document(name, arg),
+    do: XmlBuilder.doc(name, arg) |> XmlBuilder.generate
+
+  def document(name, attrs, content),
+    do: XmlBuilder.doc(name, attrs, content) |> XmlBuilder.generate
+
+  def element(name, arg),
+    do: XmlBuilder.element(name, arg) |> XmlBuilder.generate
+
+  def element(name, attrs, content),
+    do: XmlBuilder.element(name, attrs, content) |> XmlBuilder.generate
+
   test "empty element" do
-    assert XmlBuilder.doc(:person) == ~s|<?xml version="1.0"><person/>|
+    assert document(:person) == ~s|<?xml version="1.0"><person/>|
   end
 
   test "element with content" do
-    assert XmlBuilder.doc(:person, "Josh") == ~s|<?xml version="1.0"><person>Josh</person>|
+    assert document(:person, "Josh") == ~s|<?xml version="1.0"><person>Josh</person>|
   end
 
   test "element with attributes" do
-    assert XmlBuilder.doc(:person, %{occupation: "Developer", city: "Montreal"}) == ~s|<?xml version="1.0"><person city="Montreal" occupation="Developer"/>|
-    assert XmlBuilder.doc(:person, %{}) == ~s|<?xml version="1.0"><person/>|
+    assert document(:person, %{occupation: "Developer", city: "Montreal"}) == ~s|<?xml version="1.0"><person city="Montreal" occupation="Developer"/>|
+    assert document(:person, %{}) == ~s|<?xml version="1.0"><person/>|
   end
 
   test "element with attributes and content" do
-    assert XmlBuilder.doc(:person, %{occupation: "Developer", city: "Montreal"}, "Josh") == ~s|<?xml version="1.0"><person city="Montreal" occupation="Developer">Josh</person>|
-    assert XmlBuilder.doc(:person, %{occupation: "Developer", city: "Montreal"}, nil) == ~s|<?xml version="1.0"><person city="Montreal" occupation="Developer"/>|
-    assert XmlBuilder.doc(:person, %{}, "Josh") == ~s|<?xml version="1.0"><person>Josh</person>|
-    assert XmlBuilder.doc(:person, %{}, nil) == ~s|<?xml version="1.0"><person/>|
+    assert document(:person, %{occupation: "Developer", city: "Montreal"}, "Josh") == ~s|<?xml version="1.0"><person city="Montreal" occupation="Developer">Josh</person>|
+    assert document(:person, %{occupation: "Developer", city: "Montreal"}, nil) == ~s|<?xml version="1.0"><person city="Montreal" occupation="Developer"/>|
+    assert document(:person, %{}, "Josh") == ~s|<?xml version="1.0"><person>Josh</person>|
+    assert document(:person, %{}, nil) == ~s|<?xml version="1.0"><person/>|
   end
 
   test "element with children" do
-    assert XmlBuilder.doc(:person, [{:name, %{id: 123}, "Josh"}]) == ~s|<?xml version="1.0"><person><name id="123">Josh</name></person>|
-    assert XmlBuilder.doc(:person, [{:first_name, "Josh"}, {:last_name, "Nussbaum"}]) == ~s|<?xml version="1.0"><person><first_name>Josh</first_name><last_name>Nussbaum</last_name></person>|
+    assert document(:person, [{:name, %{id: 123}, "Josh"}]) == ~s|<?xml version="1.0"><person><name id="123">Josh</name></person>|
+    assert document(:person, [{:first_name, "Josh"}, {:last_name, "Nussbaum"}]) == ~s|<?xml version="1.0"><person><first_name>Josh</first_name><last_name>Nussbaum</last_name></person>|
   end
 
   test "element with attributes and children" do
-    assert XmlBuilder.doc(:person, %{id: 123}, [{:name, "Josh"}]) == ~s|<?xml version="1.0"><person id="123"><name>Josh</name></person>|
-    assert XmlBuilder.doc(:person, %{id: 123}, [{:first_name, "Josh"}, {:last_name, "Nussbaum"}]) == ~s|<?xml version="1.0"><person id="123"><first_name>Josh</first_name><last_name>Nussbaum</last_name></person>|
+    assert document(:person, %{id: 123}, [{:name, "Josh"}]) == ~s|<?xml version="1.0"><person id="123"><name>Josh</name></person>|
+    assert document(:person, %{id: 123}, [{:first_name, "Josh"}, {:last_name, "Nussbaum"}]) == ~s|<?xml version="1.0"><person id="123"><first_name>Josh</first_name><last_name>Nussbaum</last_name></person>|
   end
 
   test "children elements" do
-    assert XmlBuilder.doc([{:name, %{id: 123}, "Josh"}]) == ~s|<?xml version="1.0"><name id="123">Josh</name>|
-    assert XmlBuilder.doc([{:first_name, "Josh"}, {:last_name, "Nussbaum"}]) == ~s|<?xml version="1.0"><first_name>Josh</first_name><last_name>Nussbaum</last_name>|
+    assert document([{:name, %{id: 123}, "Josh"}]) == ~s|<?xml version="1.0"><name id="123">Josh</name>|
+    assert document([{:first_name, "Josh"}, {:last_name, "Nussbaum"}]) == ~s|<?xml version="1.0"><first_name>Josh</first_name><last_name>Nussbaum</last_name>|
   end
 
   test "quoting and escaping attributes" do
-    assert XmlBuilder.element(:person, %{height: 12}) == ~s|<person height="12"/>|
-    assert XmlBuilder.element(:person, %{height: ~s|10'|}) == ~s|<person height="10'"/>|
-    assert XmlBuilder.element(:person, %{height: ~s|10"|}) == ~s|<person height='10"'/>|
-    assert XmlBuilder.element(:person, %{height: ~s|<10'5"|}) == ~s|<person height="&lt;10'5&quot;"/>|
+    assert element(:person, %{height: 12}) == ~s|<person height="12"/>|
+    assert element(:person, %{height: ~s|10'|}) == ~s|<person height="10'"/>|
+    assert element(:person, %{height: ~s|10"|}) == ~s|<person height='10"'/>|
+    assert element(:person, %{height: ~s|<10'5"|}) == ~s|<person height="&lt;10'5&quot;"/>|
   end
 
   test "escaping content" do
-    assert XmlBuilder.element(:person, "Josh") == "<person>Josh</person>"
-    assert XmlBuilder.element(:person, "<Josh>") == "<person>&lt;Josh&gt;</person>"
+    assert element(:person, "Josh") == "<person>Josh</person>"
+    assert element(:person, "<Josh>") == "<person>&lt;Josh&gt;</person>"
   end
 end
