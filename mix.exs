b9dce62a67cd5205d1ffb2f2707794c63c4f216c
Josh Nussbaum <joshnuss@gmail.com>
2017-05-12 20:26:39 -0400

    Bump version to 0.1.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule XmlBuilder.Mixfile do
 
   def project do
     [app: :xml_builder,
-     version: "0.0.9",
+     version: "0.1.0",
      elixir: ">= 0.14.0",
      deps: deps(),
      package: [
