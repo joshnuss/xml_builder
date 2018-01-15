213469cea0e0eec8f89039166bdbeb9a03f51b46
Josh Nussbaum <joshnuss@gmail.com>
2018-01-14 23:49:43 -0500

    Fixes indentation

--- lib/xml_builder/format/indented.ex
+++ lib/xml_builder/format/indented.ex
@@ -1,9 +1,9 @@
 defmodule XmlBuilder.Format.Indented do
-   def indentation(level, options) do
-      whitespace = Keyword.get(options, :whitespace, "  ")
+  def indentation(level, options) do
+    whitespace = Keyword.get(options, :whitespace, "  ")
 
-      String.duplicate(whitespace, level)
-   end
+    String.duplicate(whitespace, level)
+  end
 
-   def line_break(), do: "\n"
+  def line_break(), do: "\n"
 end
