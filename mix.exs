7f77c3792bd63d066116b8df7a0c8ea974351eb1
devstopfix <devstopfix@gmail.com>
2020-11-21 16:30:10 +0000

    chore: elixir 1.6-1.11 in CI (#5)
    1.6 is the oldest supported Elixir version
    
    Fixed compile warning on doc.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule XmlBuilder.Mixfile do
     [
       app: :xml_builder,
       version: "2.1.2",
-      elixir: ">= 0.14.0",
+      elixir: "~> 1.6",
       deps: deps(),
       package: [
         maintainers: ["Joshua Nussbaum"],
@@ -18,22 +18,10 @@ defmodule XmlBuilder.Mixfile do
     ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: []]
   end
 
-  # Dependencies can be hex.pm packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [{:ex_doc, github: "elixir-lang/ex_doc", only: :dev}]
   end
