01c220360c539924dc9eadadba15b7f639bbde6e
Michael Neumann <mneumann@ntecs.de>
2025-03-09 09:49:23 +0100

    Remove surplus newlines (fixes #59)
    Do a `flat_map_reduce` over the list of children and filter out any
    child that is either `[]` or `nil`. The `count` is required to properly
    detect the first non-blank item, which shouldn't be prefixed with a
    new-line.
    
    As the code is used in both `format` and `format_content`, factor it out
    into `format_children`.
    
    Fixes: #59

--- lib/xml_builder.ex
+++ lib/xml_builder.ex
@@ -257,8 +257,7 @@ defmodule XmlBuilder do
     do: format({nil, nil, string}, level, options)
 
   defp format(list, level, options) when is_list(list) do
-    formatter = formatter(options)
-    map_intersperse(list, formatter.line_break(), &format(&1, level, options))
+    format_children(list, level, options)
   end
 
   defp format({nil, nil, name}, level, options) when is_bitstring(name),
@@ -339,6 +338,27 @@ defmodule XmlBuilder do
     ]
   end
 
+  defp format_children(list, level, options) when is_list(list) do
+    format_char = formatter(options).line_break()
+
+    {result, _} =
+      Enum.flat_map_reduce(list, 0, fn
+        [], count ->
+          {[], count}
+
+        nil, count ->
+          {[], count}
+
+        elm, 0 ->
+          {[format(elm, level, options)], 1}
+
+        elm, count ->
+          {[format_char, format(elm, level, options)], count + 1}
+      end)
+
+    result
+  end
+
   defp elements_with_prolog([first | rest]) when length(rest) > 0,
     do: [first_element(first) | element(rest)]
 
@@ -360,7 +380,7 @@ defmodule XmlBuilder do
 
   defp format_content(children, level, options) when is_list(children) do
     format_char = formatter(options).line_break()
-    [format_char, map_intersperse(children, format_char, &format(&1, level, options))]
+    [format_char, format_children(children, level, options)]
   end
 
   defp format_content(content, _level, _options),
