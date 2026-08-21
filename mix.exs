23c91545295c7abf46439b9af0967769e1782463
Josh Nussbaum <joshnuss@gmail.com>
2026-08-20 23:44:37 -0400

    Bump to 2.4.1

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule XmlBuilder.Mixfile do
   def project do
     [
       app: :xml_builder,
-      version: "2.4.0",
+      version: "2.4.1",
       elixir: "~> 1.12",
       deps: deps(),
       docs: docs(),
